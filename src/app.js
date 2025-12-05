f635b812c9c339d87e6369017d7ec114dc47882b
veronica860108 <veroruiba@gmail.com>
2025-12-05 19:23:05 +0000

    carta aleatoria

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// ---------------------------------funcion aleatorio-----------------------------------------
+const numeroAleatoriomath = (array) => {
+  const indiceRandom = Math.floor(Math.random() * array.length)
+  // console.log(array[indiceRandom]);
+  return array[indiceRandom];
+}
+
+window.onload = function () {
+
+  const iconos = ['♦', '♥', '♠', '♣'];
+  const numeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+
+  let palos = document.getElementById('palo');
+  let numeroCont = document.getElementById('numero');
+  let palosinvertido = document.getElementById(`palosReves`);
+
+  palos.textContent = numeroAleatoriomath(iconos);
+  numeroCont.textContent = numeroAleatoriomath(numeros);
+  palosinvertido.textContent = numeroAleatoriomath(iconos);
+
+}
