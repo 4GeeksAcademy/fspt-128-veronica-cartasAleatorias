31da5a3cb25fee040b6594954f72fcd17fe95cbe
veronica860108 <veroruiba@gmail.com>
2025-12-05 22:26:57 +0000

    v2cartaAleatoria

--- src/app.js
+++ src/app.js
@@ -5,12 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// ---------------------------------funcion aleatorio-----------------------------------------
-const numeroAleatoriomath = (array) => {
-  const indiceRandom = Math.floor(Math.random() * array.length)
-  // console.log(array[indiceRandom]);
-  return array[indiceRandom];
-}
+
 
 window.onload = function () {
 
@@ -18,12 +13,22 @@ window.onload = function () {
   const numeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 
 
-  let palos = document.getElementById('palo');
+  let palos = document.getElementById(`palo`);
   let numeroCont = document.getElementById('numero');
-  let palosinvertido = document.getElementById(`palosReves`);
+  let palo2 = document.getElementById(`paloReves`)
+  let elIcono = numeroAleatoriomath(iconos);
+  // console.log(elIcono);
 
-  palos.textContent = numeroAleatoriomath(iconos);
+
+  palos.textContent = elIcono;
   numeroCont.textContent = numeroAleatoriomath(numeros);
-  palosinvertido.textContent = numeroAleatoriomath(iconos);
+  palo2.textContent = elIcono;
 
+}
+// ---------------------------------funcion aleatorio-----------------------------------------
+const numeroAleatoriomath = (array) => {
+  const indiceRandom = Math.floor(Math.random() * array.length)
+  // console.log(array[indiceRandom]);
+  return array[indiceRandom];
+  ;
 }
