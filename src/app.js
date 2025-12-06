f589f200f966a770d06337b85df5c405c5b5e57a
veronica860108 <veroruiba@gmail.com>
2025-12-06 18:01:15 +0000

    vfinal

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function () {
 
-  const iconos = ['♦', '♥', '♠', '♣'];
+  const iconos = ["♦", "♥", "♠", "♣"];
   const numeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 
 
@@ -17,12 +17,40 @@ window.onload = function () {
   let numeroCont = document.getElementById('numero');
   let palo2 = document.getElementById(`paloReves`)
   let elIcono = numeroAleatoriomath(iconos);
+  let elboton = document.querySelector(".boton");
+  let color = `negro`
+
+
+elboton.addEventListener(`click`,()=>{location.reload()})
+
+
   // console.log(elIcono);
+  if (elIcono ==="♦" ) {
+    color = `rojo`
+    console.log(elIcono);
+    console.log(color);
+    
+  }else if(elIcono==="♥") {
+    color = `rojo`
+    console.log(color);
+    
+  }
+// console.log(color);
 
+  palos.innerHTML =`<h2 class="${color}"> ${elIcono}</h2>`;
 
-  palos.textContent = elIcono;
   numeroCont.textContent = numeroAleatoriomath(numeros);
-  palo2.textContent = elIcono;
+  palo2.innerHTML= `<h2 class="${color}"> ${elIcono}</h2>`;
+
+  // coloricono.textContent=cambiarcolor(`palo`);
+
+  // let colorCambiado = () => {
+
+  // }
+
+
+
+
 
 }
 // ---------------------------------funcion aleatorio-----------------------------------------
